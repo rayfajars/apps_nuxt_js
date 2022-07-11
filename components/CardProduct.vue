358332354c00d6cd9ef44f4be08451a3f3e27116
Ray Fajar S <rayfajars.com>
2022-07-11 20:27:34 +0700

    add component and fetch api

---
+++ components/CardProduct.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="product-card">
+    <div class="product-tumb">
+      <img :src="product.image" alt="" />
+    </div>
+    <div class="product-details">
+      <span class="product-catagory">{{ product.category }}</span>
+      <h4>
+        <a href="">{{ product.title }}</a>
+      </h4>
+      <p class="limit-description">
+        {{ product.description }}
+      </p>
+      <div class="product-bottom-details">
+        <div class="product-price">${{product.price}}</div>
+        <div class="product-links">
+          <a href=""><i class="fa fa-heart"></i></a>
+          <a href=""><i class="fa fa-shopping-cart"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardProduct",
+  props: ["product"],
+};
+</script>
+
+<style>
+.product-card {
+  width: 380px;
+  position: relative;
+  box-shadow: 0 2px 7px #dfdfdf;
+  margin: 50px auto;
+  background: #fafafa;
+}
+
+.badge {
+  position: absolute;
+  left: 0;
+  top: 20px;
+  text-transform: uppercase;
+  font-size: 13px;
+  font-weight: 700;
+  background: red;
+  color: #fff;
+  padding: 3px 10px;
+}
+
+.product-tumb {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 300px;
+  padding: 50px;
+  background: #fff;
+}
+
+.product-tumb img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.product-details {
+  padding: 30px;
+}
+
+.product-catagory {
+  display: block;
+  font-size: 12px;
+  font-weight: 700;
+  text-transform: uppercase;
+  color: #ccc;
+  margin-bottom: 18px;
+}
+
+.product-details h4 a {
+  font-weight: 500;
+  display: block;
+  margin-bottom: 18px;
+  text-transform: uppercase;
+  color: #363636;
+  text-decoration: none;
+  transition: 0.3s;
+}
+
+.product-details h4 a:hover {
+  color: #fbb72c;
+}
+
+.product-details p {
+  font-size: 15px;
+  line-height: 22px;
+  margin-bottom: 18px;
+  color: #999;
+}
+
+.product-bottom-details {
+  overflow: hidden;
+  border-top: 1px solid #eee;
+  padding-top: 20px;
+}
+
+.product-bottom-details div {
+  float: left;
+  width: 50%;
+}
+
+.product-price {
+  font-size: 18px;
+  color: #fbb72c;
+  font-weight: 600;
+}
+
+.product-price small {
+  font-size: 80%;
+  font-weight: 400;
+  text-decoration: line-through;
+  display: inline-block;
+  margin-right: 5px;
+}
+
+.product-links {
+  text-align: right;
+}
+
+.product-links a {
+  display: inline-block;
+  margin-left: 5px;
+  color: #e1e1e1;
+  transition: 0.3s;
+  font-size: 17px;
+}
+
+.product-links a:hover {
+  color: #fbb72c;
+}
+
+.limit-description {
+  white-space: nowrap;
+  width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  /* border: 1px solid #000000; */
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,43 @@
 <template>
-  <Tutorial/>
+  <!-- <div>
+    {{ products }}
+    <CardProduct />
+  </div> -->
+
+  <div>
+    <p v-if="$fetchState.pending">Loading....</p>
+    <p v-else-if="$fetchState.error">Error while fetching mountains</p>
+    <div v-else>
+      <!-- <li v-for="(mountain, index) in mountains" :key="index">
+        {{ mountain.title }}
+      </li> -->
+      <template v-for="(product, index) in products" >
+        <CardProduct :product="product"/>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  data() {
+    return {
+      products: [],
+    };
+  },
+  // methods: {
+  // async fetchSomething() {
+  //   const ip = await this.$axios.$get("https://fakestoreapi.com/products");
+  //   this.products = ip;
+  // },
+
+  async fetch() {
+    this.products = await fetch("https://fakestoreapi.com/products").then(
+      (res) => res.json()
+    );
+
+    console.log(this.products)
+  },
+};
 </script>
