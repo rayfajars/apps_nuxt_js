83f832c069812276e1bf3b49a7212b42c784f5e4
Ray Fajar S <rayfajars.com>
2022-07-12 13:04:00 +0700

    add aset add category

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,27 @@
 <template>
-  <!-- <div>
-    {{ products }}
-    <CardProduct />
-  </div> -->
+  <div class="container">
+    <button
+      class="button button-category"
+      :class="getClickCategory == '' ? 'button-selected' : ''"
+      @click="categoryClick('')"
+    >
+      All
+    </button>
+    <template v-for="(category, index) in categorys">
+      <button
+        class="button button-category"
+        :class="getClickCategory == category ? 'button-selected' : ''"
+        @click="categoryClick(category)"
+      >
+        {{ category }}
+      </button>
+    </template>
 
-  <div>
-    <p v-if="$fetchState.pending">Loading....</p>
-    <p v-else-if="$fetchState.error">Error while fetching mountains</p>
-    <div v-else>
-      <!-- <li v-for="(mountain, index) in mountains" :key="index">
-        {{ mountain.title }}
-      </li> -->
-      <template v-for="(product, index) in products" >
-        <CardProduct :product="product"/>
+    <div class="grid-container">
+      <template v-for="(product, index) in products">
+        <div class="column">
+          <CardProduct :product="product" />
+        </div>
       </template>
     </div>
   </div>
@@ -24,20 +33,43 @@ export default {
   data() {
     return {
       products: [],
+      categorys: [],
+      limit: 8,
+      apiSelected: "",
+      apiProducts: `https://fakestoreapi.com/products`,
+      apiCategory: "https://fakestoreapi.com/products/categories",
+      apiProductSelectedCategory: `https://fakestoreapi.com/products/category/`,
+      getClickCategory: "",
     };
   },
-  // methods: {
-  // async fetchSomething() {
-  //   const ip = await this.$axios.$get("https://fakestoreapi.com/products");
-  //   this.products = ip;
-  // },
+  methods: {
+    async getProduct() {
+      const data = await this.$axios.$get(this.apiSelected) 
+      if(data){
+        this.products = data;
+      }else {
+        console.log('reload')
+      }
+    },
+    async getCategory() {
+      this.categorys = await this.$axios.$get(this.apiCategory)
+    },
+    categoryClick(data) {
+      if (data) {
+        this.apiSelected =
+          this.apiProductSelectedCategory + data + `?limit=${this.limit}`;
+      } else {
+        this.apiSelected = this.apiProducts + `?limit=${this.limit}`;
+      }
 
-  async fetch() {
-    this.products = await fetch("https://fakestoreapi.com/products").then(
-      (res) => res.json()
-    );
-
-    console.log(this.products)
+      this.getClickCategory = data;
+      this.getProduct();
+    },
+  },
+  mounted() {
+    this.apiSelected = this.apiProducts + `?limit=${this.limit}`;
+    this.getCategory();
+    this.getProduct();
   },
 };
 </script>
